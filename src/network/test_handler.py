ec6341ff6b693b12684ffb6cb0e73931052e05bc
Sid Bhattacharya <bhatta70@msu.edu>
2024-02-27 10:14:41 -0500

    better test file

--- src/network/test_handler.py
+++
@@ -1,5 +0,0 @@
-import requests
-
-data = {"key1": 0, "key2": 100}
-response = requests.post('http://localhost:8000', data=data)
-print(response.content)
--- src/network/test_listener.py
+++ src/network/test_listener.py
@@ -1,7 +1,20 @@
 import listen
+import threading
+import requests
+import time
+import random
 models=[]
 def callback(data):
     global models
     models.append(data)
     print(models)
-listen.listen_for_models(timeout=30, host='localhost', port=8000, callback=callback)
+def test_handler():
+    data = {"key1": random.random(), "key2": random.random()}
+    response = requests.post('http://localhost:8000', data=data)
+    print(response.content)
+thread = threading.Thread(target=listen.listen_for_models, args=(30, 8000, 'localhost', callback))
+time.sleep(1)
+thread.start()
+test_handler()
+
+# if data shows up in the terminal, the test passes
