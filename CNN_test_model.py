97e7cbe0ccc2ddae25858acb7d9e3369df25d5a7
roohaan111 <70602076+roohaan111@users.noreply.github.com>
2024-03-30 00:29:29 -0400

    Update CNN_test_model.py to handle MNIST data

--- CNN_test_model.py
+++ CNN_test_model.py
@@ -1,50 +1,23 @@
-import pandas as pd
-import csv
-import cv2
-import os
-import numpy as np
-import PIL
+import matplotlib.pyplot as plt
 import tensorflow as tf
-from tensorflow import keras
-from tensorflow.keras import layers
-from tensorflow.keras.models import Sequential
-from sklearn.model_selection import train_test_split
-from keras.utils import img_to_array
+from tensorflow.keras import layers, Sequential
 
-image_size = (180, 180)
-batch_size = 128
+(x_train, y_train), (x_test, y_test) = tf.keras.datasets.mnist.load_data()
 
-print(2)
+x_train, x_test = x_train / 255.0, x_test / 255.0
 
-val_ds = tf.keras.utils.image_dataset_from_directory(
-    "dataset",
-    labels="inferred",
-    label_mode="int",
-    validation_split=0.2,
-    subset = "validation",
-    seed = 123,
-    image_size=image_size,
-    batch_size=batch_size,
-)
+x_train = x_train[..., tf.newaxis]
+x_test = x_test[..., tf.newaxis]
 
-train_ds = tf.keras.utils.image_dataset_from_directory("dataset",
-  validation_split=0.2,
-  subset="training",
-  seed=123,
-  image_size=(180, 180),
-  batch_size=batch_size)
-
-class_names = train_ds.class_names
+train_ds = tf.data.Dataset.from_tensor_slices((x_train, y_train)).shuffle(10000).batch(128)
+val_ds = tf.data.Dataset.from_tensor_slices((x_test, y_test)).batch(128)
 
 AUTOTUNE = tf.data.AUTOTUNE
-
-train_ds = train_ds.cache().shuffle(1000).prefetch(buffer_size=AUTOTUNE)
+train_ds = train_ds.cache().prefetch(buffer_size=AUTOTUNE)
 val_ds = val_ds.cache().prefetch(buffer_size=AUTOTUNE)
-normalization_layer = layers.Rescaling(1./255)
-num_classes = len(class_names)
-img_height, img_width = 180, 180
+
 model = Sequential([
-  layers.Rescaling(1./255, input_shape=(img_height, img_width, 3)),
+  layers.Rescaling(1./255, input_shape=(28, 28, 1)),
   layers.Conv2D(16, 3, padding='same', activation='relu'),
   layers.MaxPooling2D(),
   layers.Conv2D(32, 3, padding='same', activation='relu'),
@@ -53,36 +26,36 @@ model = Sequential([
   layers.MaxPooling2D(),
   layers.Flatten(),
   layers.Dense(128, activation='relu'),
-  layers.Dense(num_classes)
+  layers.Dense(10) # 10 classes for the digits 0-9
 ])
+
 model.compile(optimizer='adam',
               loss=tf.keras.losses.SparseCategoricalCrossentropy(from_logits=True),
               metrics=['accuracy'])
-epochs=10
+
+epochs = 5
 history = model.fit(
   train_ds,
   validation_data=val_ds,
   epochs=epochs
 )
 
-acc = history.history['accuracy']
-val_acc = history.history['val_accuracy']
+# acc = history.history['accuracy']
+# val_acc = history.history['val_accuracy']
+# loss = history.history['loss']
+# val_loss = history.history['val_loss']
+# epochs_range = range(epochs)
 
-loss = history.history['loss']
-val_loss = history.history['val_loss']
+# plt.figure(figsize=(8, 8))
+# plt.subplot(1, 2, 1)
+# plt.plot(epochs_range, acc, label='Training Accuracy')
+# plt.plot(epochs_range, val_acc, label='Validation Accuracy')
+# plt.legend(loc='lower right')
+# plt.title('Training and Validation Accuracy')
 
-epochs_range = range(epochs)
-
-plt.figure(figsize=(8, 8))
-plt.subplot(1, 2, 1)
-plt.plot(epochs_range, acc, label='Training Accuracy')
-plt.plot(epochs_range, val_acc, label='Validation Accuracy')
-plt.legend(loc='lower right')
-plt.title('Training and Validation Accuracy')
-
-plt.subplot(1, 2, 2)
-plt.plot(epochs_range, loss, label='Training Loss')
-plt.plot(epochs_range, val_loss, label='Validation Loss')
-plt.legend(loc='upper right')
-plt.title('Training and Validation Loss')
-plt.show()
+# plt.subplot(1, 2, 2)
+# plt.plot(epochs_range, loss, label='Training Loss')
+# plt.plot(epochs_range, val_loss, label='Validation Loss')
+# plt.legend(loc='upper right')
+# plt.title('Training and Validation Loss')
+# plt.show()
